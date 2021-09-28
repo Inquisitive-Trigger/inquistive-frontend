e416d5e6493bbeab155190ce7c0f60e6def7815b
alvinend <alvinendratno@live.com>
2021-09-28 04:29:25 +0000

    Connect Introduce Detail Page

--- src/app/services/projectService.ts
+++ src/app/services/projectService.ts
@@ -15,6 +15,19 @@ export type Project = {
   company_name: string
 }
 
+export const createEmptyProject = () => ({
+  id: 999,
+  name: '',
+  status: '',
+  concept: '',
+  deadline: new Date(),
+  reward: '',
+  created_at: new Date(),
+  updated_at: new Date(),
+  category: '',
+  company_name: ''
+}) 
+
 export const fetchProjectList = async() => {
   const res = await axios.get('/projects')
 
@@ -25,4 +38,10 @@ export const searchProjects = async (category: string) => {
   const res = await axios.get(`/projects/search?category=${category}`)
 
   return res.data.projects as Project[]
+}
+
+export const fetchProject = async (id: string) => {
+  const res = await axios.get(`/projects/${id}`)
+
+  return res.data.project as Project
 }
--- src/container/page/IntroducerDetailPageContainer.tsx
+++ src/container/page/IntroducerDetailPageContainer.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react'
 import { useParams } from 'react-router'
+import { createEmptyProject, fetchProject, Project } from '../../app/services/projectService'
 import { IntroducerDetailPage } from '../../component/page/IntroducerDetailPage'
+import { toast } from 'react-toastify'
 
 export const IntroducerDetailPageContainer = () => {
+  const [project, setProject] = React.useState<Project>(createEmptyProject())
+  const { id } = useParams<{ id:string }>()
 
-  return <IntroducerDetailPage />
+  React.useEffect(
+    () => {
+      (async () => {
+        try {
+          const project = await fetchProject(id)
+          setProject(project)
+        } catch {
+          toast.error('案件取得が失敗しました')
+        }
+      })()
+    },
+    []
+  )
+
+
+  return <IntroducerDetailPage
+    project={project}
+  />
 }
